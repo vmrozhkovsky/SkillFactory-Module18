a116b9d5e93274a7d634b75a06a4f0296416821c
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-08-05 23:34:38 +0300

    Initial commit

---
+++ FinalPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+Console.WriteLine("Введите адрес видео:");
+string videoUrl = Console.ReadLine();
+string outputFilePath = @"C:\Video\test";
+var youtubeclient = new YoutubeClient();
+var video = await youtubeclient.Videos.GetAsync(videoUrl);
+Console.WriteLine($"Название: {video.Title}");
+Console.WriteLine($"Продолжительность: {video.Duration}");
+Console.WriteLine($"Автор: {video.Author}");
